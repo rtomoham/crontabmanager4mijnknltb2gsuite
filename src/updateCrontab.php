a173928a2d8841efe95a01784cc29432b5782175
rtomoham <rtomoham@cisco.com>
2020-06-18 10:29:23 +0200

    Create updateCrontab.php

---
+++ src/updateCrontab.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once("Utils.php")
+
+$manager = new CrontabManager();
+
+$settings = getSettings();
+$cron = $settings[STRING_CRON];
+
+$timecode = '';
+foreach ($cron as $key=>$item) {
+  if (0 >= strlen($item)) {
+    $cron[$key] = '*';
+  }
+  $timecode .= $cron[$key] . ' ';
+}
+$timecode = substr($timecode, 0, -1);
+                
+		$onMinute = $cron[STRING_ON_MINUTE]; 
+		$onHour = $cron[STRING_ON_HOUR];
+		$onDayOfMonth = $cron[STRING_ON_DAY_OF_MONTH];
+		$onMonth = $cron[STRING_ON_MONTH];
+		$onDayOfWeek = $cron[STRING_ON_DAY_OF_WEEK];
+
+$job = $manager->newJob();
+$job->on($timecode);
+$job->doJob('php /mijnknltb2gsuite/refresh.php');
+$manager->add($job);
+$manager->save(False);
+
+?>
