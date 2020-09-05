d18090893881e3306a49e65e01826c2deeb5bf36
rtomoham <robert@tomohamat.com>
2020-09-05 15:41:35 +0200

    Changed statics to public const

--- src/updateCrontab.php
+++ src/updateCrontab.php
@@ -24,11 +24,11 @@ foreach ($cron as $key=>$item) {
 }
 $timecode = substr($timecode, 0, -1);
                 
-$onMinute = $cron[Settings::STRING_ON_MINUTE]; 
-$onHour = $cron[Settings::STRING_ON_HOUR];
-$onDayOfMonth = $cron[Settings::STRING_ON_DAY_OF_MONTH];
-$onMonth = $cron[Settings::STRING_ON_MONTH];
-$onDayOfWeek = $cron[Settings::STRING_ON_DAY_OF_WEEK];
+$onMinute = $cron[$ettings->STRING_ON_MINUTE]; 
+$onHour = $cron[$ettings->STRING_ON_HOUR];
+$onDayOfMonth = $cron[$ettings->STRING_ON_DAY_OF_MONTH];
+$onMonth = $cron[$ettings->STRING_ON_MONTH];
+$onDayOfWeek = $cron[$ettings->STRING_ON_DAY_OF_WEEK];
 
 $job = $manager->newJob();
 $job->on($timecode);
