c7c2e0315fc43c80b1c2611aedf84f672ba61da3
rtomoham <robert@tomohamat.com>
2020-09-05 15:31:01 +0200

    Minor bug fixes

--- src/updateCrontab.php
+++ src/updateCrontab.php
@@ -3,7 +3,7 @@
 require_once('Utils.php');
 require_once('CrontabManager.php');
 require_once('CronEntry.php');
-require_once('Settings.php);
+require_once('Settings.php');
 
 $manager = new CrontabManager();
 $settings = Settings::getInstance();
@@ -24,11 +24,12 @@ foreach ($cron as $key=>$item) {
 }
 $timecode = substr($timecode, 0, -1);
                 
-		$onMinute = $cron[STRING_ON_MINUTE]; 
-		$onHour = $cron[STRING_ON_HOUR];
-		$onDayOfMonth = $cron[STRING_ON_DAY_OF_MONTH];
-		$onMonth = $cron[STRING_ON_MONTH];
-		$onDayOfWeek = $cron[STRING_ON_DAY_OF_WEEK];
+$onMinute = $cron[Settings::STRING_ON_MINUTE]; 
+$onHour = $cron[Settings::STRING_ON_HOUR];
+$onDayOfMonth = $cron[Settings::STRING_ON_DAY_OF_MONTH];
+$onMonth = $cron[Settings::STRING_ON_MONTH];
+$onDayOfWeek = $cron[Settings::STRING_ON_DAY_OF_WEEK];
+
 $job = $manager->newJob();
 $job->on($timecode);
 $job->doJob('php ' . $settings->getProgramPath() . 'refresh.php > ' . $settings->getDataPath() . 'output.txt');
